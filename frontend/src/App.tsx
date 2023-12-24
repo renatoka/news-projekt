9da6f6717737fab59e5dfa361e3825103411af9b
Renato <rkauric@vvg.hr>
2023-12-24 11:26:56 +0100

    added new screens

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { NotFound } from './components/NotFound';
 import { DashboardLayout } from './layouts/DashboardLayout';
 import { AdminLayout } from './layouts/AdminLayout';
-import { Dashboard } from './pages/Dashboard/Dashboard';
+import { Dashboard } from './pages/Unauthorized/Dashboard/Dashboard';
 import { SingleArticle } from './components/Article';
-import { ArticlesPanel } from './pages/Admins/Articles/ArticlesPanel';
-import { UsersPanel } from './pages/Admins/Users/UsersPanel';
-import { UserProfile } from './pages/Admins/User/UserProfile';
-import { CommentsPanel } from './pages/Admins/Comments/CommentsPanel';
+import { ArticlesPanel } from './pages/Authorized/Admins/Panels/Articles/ArticlesPanel';
+import { UsersPanel } from './pages/Authorized/Admins/Panels/Users/UsersPanel';
+import { UserProfile } from './pages/Authorized/Admins/Others/User/UserProfile';
+import { CommentsPanel } from './pages/Authorized/Admins/Panels/Comments/CommentsPanel';
+import { Suspense } from 'react';
+import { Login } from './pages/Auth/Login';
+import { Register } from './pages/Auth/Register';
 
 function App() {
   const news_categories = [
@@ -55,27 +58,31 @@ function App() {
   ];
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<DashboardLayout />}>
-          <Route path="/:category/:slug/:id" element={<SingleArticle />} />
-          {news_categories.map((category) => (
-            <Route
-              key={category.id}
-              path={category.path}
-              element={<Dashboard />}
-            />
-          ))}
-        </Route>
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route path="articles" element={<ArticlesPanel />} />
-          <Route path="comments" element={<CommentsPanel />} />
-          <Route path="users" element={<UsersPanel />} />
-          <Route path="user/:id" element={<UserProfile />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DashboardLayout />}>
+            <Route path="/:category/:slug/:id" element={<SingleArticle />} />
+            {news_categories.map((category) => (
+              <Route
+                key={category.id}
+                path={category.path}
+                element={<Dashboard />}
+              />
+            ))}
+          </Route>
+          <Route path="/admin" element={<AdminLayout />}>
+            <Route path="articles" element={<ArticlesPanel />} />
+            <Route path="comments" element={<CommentsPanel />} />
+            <Route path="users" element={<UsersPanel />} />
+            <Route path="user/:id" element={<UserProfile />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
