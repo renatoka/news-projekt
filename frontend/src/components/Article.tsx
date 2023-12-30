9a3abe166eee6b156646966359d8a2c6f1669eab
Renato <rkauric@vvg.hr>
2023-12-30 16:41:45 +0100

    create comment functionality

--- frontend/src/components/Article.tsx
+++ frontend/src/components/Article.tsx
@@ -5,25 +5,102 @@ import { Article } from '../../../src/handlers/articles/entities/article.entity'
 import { getArticle } from '../redux/actions/articles.actions';
 import { useAppDispatch, useAppSelector } from '../redux/hooks';
 import { SingleComment } from './SingleComment';
+import { createComment } from '../redux/actions/comments.actions';
 
 export const SingleArticle = () => {
   const dispatch = useAppDispatch();
   const { id } = useParams<{ slug: string; id: string }>();
+  const [notLoggedIn, setNotLoggedIn] = useState<boolean>(false);
   const [news_article, setNewsArticle] = useState<Article>();
+  const [errors, setErrors] = useState<{
+    content: string;
+    user_id: string;
+  }>({
+    content: '',
+    user_id: '',
+  });
+  const [inputs, setInputs] = useState<{
+    content: string;
+    user_id: string;
+    article_id: number;
+  }>({
+    content: '',
+    user_id: '',
+    article_id: 0,
+  });
 
   const { article, loading } = useAppSelector((state) => state.article);
+  const { user } = useAppSelector((state) => state.loggedUser);
+  const { success } = useAppSelector((state) => state.createComment);
 
   useEffect(() => {
     if (id) {
       dispatch(getArticle(+id));
+      setInputs((prev) => ({
+        ...prev,
+        article_id: +id,
+      }));
     }
   }, [id, dispatch]);
 
   useEffect(() => {
+    if (user) {
+      setInputs((prev) => ({
+        ...prev,
+        user_id: user.id,
+      }));
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (!user) {
+      setNotLoggedIn(true);
+    }
+  }, [user]);
+
+  useEffect(() => {
     if (article) {
-      setNewsArticle(article as Article);
+      setNewsArticle(article);
     }
   }, [article]);
+
+  useEffect(() => {
+    if (success) {
+      setTimeout(() => {
+        dispatch({ type: 'CREATE_COMMENT_RESET' });
+        window.location.reload();
+      }, 2000);
+    }
+  }, [success, dispatch]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    let errorsObj: {
+      content: string;
+      user_id: string;
+    } = {
+      content: '',
+      user_id: '',
+    };
+    for (const [key, value] of Object.entries(inputs)) {
+      if (!value) {
+        errorsObj[key as keyof typeof errorsObj] = 'This field is required';
+      }
+    }
+    for (const [_, value] of Object.entries(errorsObj)) {
+      if (value) {
+        setErrors(errorsObj);
+        return;
+      }
+    }
+    dispatch(createComment(inputs));
+  };
 
   return (
     <div className="flex flex-col gap-5">
@@ -65,20 +142,36 @@ export const SingleArticle = () => {
         <div className="flex flex-col gap-5">
           <div className="flex flex-col w-full">
             <Skeleton isLoaded={!loading}>
-              <h1 className="text-2xl font-bold mb-5">
-                Comments {news_article?.comments?.length}
-              </h1>
-              {news_article?.comments?.map((comment: any) => (
-                <SingleComment comment={comment} key={comment.id} />
-              ))}
+              {news_article?.comments?.length != 0 && (
+                <>
+                  <h1 className="text-2xl font-bold mb-5">
+                    Comments {news_article?.comments?.length}
+                  </h1>
+                  {news_article?.comments?.map((comment: any) => (
+                    <SingleComment key={comment.id} comment={comment} />
+                  ))}
+                </>
+              )}
               <div className="flex flex-col gap-5 mb-5">
                 <h1 className="text-2xl font-bold">Add comment</h1>
                 <Textarea
                   placeholder="Add comment"
                   width="100%"
                   height="100px"
+                  onChange={handleChange}
+                  name="content"
+                  value={inputs.content}
+                  errorMessage={errors.content}
+                  isRequired
+                  required
                 />
-                <Button color="primary">Add comment</Button>
+                <Button
+                  color="primary"
+                  onClick={handleSubmit}
+                  isDisabled={notLoggedIn}
+                >
+                  {notLoggedIn ? 'You must be logged in to comment' : 'Submit'}
+                </Button>
               </div>
             </Skeleton>
           </div>
